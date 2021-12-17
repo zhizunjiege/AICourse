33157b5d0656764c187d29db358e6e3e48a15669
zhizunjiege <2452816044@qq.com>
2021-12-17 21:32:29 +0800

    新增绘图功能

--- tests/test_EM4GMM.py
+++ tests/test_EM4GMM.py
@@ -1,10 +1,12 @@
 from src.em import EM4GMM
 
 if __name__ == "__main__":
+    from matplotlib import pyplot as plt
     import numpy as np
+    from scipy.stats import norm
 
-    samples = 10000  # 数据量
-    p, m1, m2, s1, s2 = 0.7, 175, 165, 5, 5  # 真实参数
+    samples = 2000  # 数据量
+    p, m1, m2, s1, s2 = 0.7, 175, 165, 3, 3  # 真实参数
 
     np.random.seed(0)  # 设定随机数种子
 
@@ -14,7 +16,7 @@ if __name__ == "__main__":
     init_pro = [0.5, 0.5]
     init_mean = [[180], [160]]  # 先验：男生比女生高
     init_cov = [[[36]], [[36]]]
-    iteration = 1000
+    iteration = 2000
     tolerance = 1e-8
 
     print('求解中...')
@@ -31,3 +33,31 @@ if __name__ == "__main__":
     print('求解完毕.')
 
     solution.print()
+
+    theta = solution.theta
+    plot_x = np.linspace(150, 190, 100)
+    plot_ytm = norm.pdf(plot_x, loc=m1, scale=s1)
+    plot_ytf = norm.pdf(plot_x, loc=m2, scale=s2)
+    plot_yt = p * plot_ytm + (1 - p) * plot_ytf
+    plot_ypm = norm.pdf(plot_x, loc=theta["mean"][0][0], scale=np.sqrt(theta["cov"][0][0, 0]))
+    plot_ypf = norm.pdf(plot_x, loc=theta["mean"][1][0], scale=np.sqrt(theta["cov"][1][0, 0]))
+    plot_yp = theta["pro"][0] * plot_ypm + theta["pro"][1] * plot_ypf
+
+    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    plt.subplot(2, 1, 1)
+    plt.title('随机数据-真实概率密度函数')
+    plt.ylabel('概率密度')
+    plt.xlabel('身高/厘米')
+    plt.plot(plot_x, plot_ytm)
+    plt.plot(plot_x, plot_ytf)
+    plt.plot(plot_x, plot_yt)
+    plt.legend(['男生', '女生', '混合'])
+    plt.subplot(2, 1, 2)
+    plt.title('随机数据-预测概率密度函数')
+    plt.ylabel('概率密度')
+    plt.xlabel('身高/厘米')
+    plt.plot(plot_x, plot_ypm)
+    plt.plot(plot_x, plot_ypf)
+    plt.plot(plot_x, plot_yp)
+    plt.legend(['男生', '女生', '混合'])
+    plt.show()
--- tests/test_EM4GMM_classmate.py
+++ tests/test_EM4GMM_classmate.py
@@ -1,18 +1,20 @@
 from src.em import EM4GMM
 
 if __name__ == "__main__":
+    from matplotlib import pyplot as plt
     import numpy as np
     import pandas as pd
+    from scipy.stats import norm
     from scipy.stats import multivariate_normal as mul_norm
 
-    np.random.seed(100)  # 设定随机数种子
+    np.random.seed(0)  # 设定随机数种子
 
     data = pd.read_csv("tests/height.csv")  # 读取数据
 
     dataset = np.array(data['身高']).reshape(-1, 1)  # 同学身高数据集
-    init_pro = [0.6667, 0.3334]
-    init_mean = [[175], [162]]  # 先验：男生比女生高
-    init_cov = [[[10]], [[10]]]
+    init_pro = [0.7, 0.3]
+    init_mean = [[180], [165]]  # 先验：男生比女生高
+    init_cov = [[[25]], [[25]]]
     iteration = 2000
     tolerance = 1e-8
 
@@ -40,3 +42,20 @@ if __name__ == "__main__":
     data.to_csv('tests/height.csv', index=None)
 
     print('性别判断结果：', genders)
+
+    theta = solution.theta
+    plot_x = np.linspace(150, 190, 100)
+    plot_ypm = norm.pdf(plot_x, loc=theta["mean"][0][0], scale=np.sqrt(theta["cov"][0][0, 0]))
+    plot_ypf = norm.pdf(plot_x, loc=theta["mean"][1][0], scale=np.sqrt(theta["cov"][1][0, 0]))
+    plot_yp = theta["pro"][0] * plot_ypm + theta["pro"][1] * plot_ypf
+
+    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    plt.figure()
+    plt.title('真实数据-预测概率密度函数')
+    plt.ylabel('概率密度')
+    plt.xlabel('身高/厘米')
+    plt.plot(plot_x, plot_ypm)
+    plt.plot(plot_x, plot_ypf)
+    plt.plot(plot_x, plot_yp)
+    plt.legend(['男生', '女生', '混合'])
+    plt.show()
